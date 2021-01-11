2daa83ebd295e67b0cc5e71768772f9f23c9a39d
distanceCount <Ywh552200>
2021-01-11 09:52:07 +0800

    测试上传

---
+++ SHOP-UPLOAD/src/test/java/Test.java
@@ -0,0 +1,14 @@
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * @ClassName: Test
+ * @Description: TODO
+ * @author: yang
+ * @date: 2021/1/11  9:51
+ */
+@Slf4j
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("测试上传");
+    }
+}
